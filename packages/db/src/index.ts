f8274acb178a11341e81e293514454588ebf679e
haeshitsa <harshitsahu3036@gmail.com>
2026-09-10 16:03:43 +0530

    feat(v1.04): export persistence boundaries

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,3 +3,7 @@ export { PrismaClient } from '@prisma/client';
 export * from '@prisma/client';
 export * from './auth.js';
 export * from './objectives.js';
+export * from './audit.js';
+export * from './approval-repository.js';
+export * from './control-plane-repository.js';
+export * from './workflow-repository.js';
