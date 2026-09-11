e8ac2867f1e54b12304b1d053f34be03445a9fe5
haeshitsa <harshitsahu3036@gmail.com>
2026-09-12 00:40:05 +0530

    fix: export control-plane delegation bridge

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -4,3 +4,4 @@ export * from './worker-runtime.js';
 export * from './job-dispatcher.js';
 export * from './orchestrator.js';
 export * from './delegation-job-adapter.js';
+export * from './delegation-entrypoint.js';
